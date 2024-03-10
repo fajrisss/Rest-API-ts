fb549ca106706c898a5af54883c6f2d37dd46168
fajisss <fajrissoba@gmail.com>
2024-03-10 10:23:37 +0700

    setup eslint, prettier, husky

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-import express, {Application, Request, Response, NextFunction} from "express";
+/* eslint-disable @typescript-eslint/no-confusing-void-expression */
+/* eslint-disable @typescript-eslint/consistent-type-imports */
+import express, { Application, Request, Response, NextFunction } from 'express'
 
-const app: Application  = express();
-const port: number = 4000;
+const app: Application = express()
+const port: number = 4000
 
-app.use("/health", (req: Request, res:Response, next:NextFunction) => {
-    res.status(200).send({ data: "200" });
-});
+app.use('/health', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ data: '200', status: '200' })
+})
 
-
-app.listen(port, () => console.log(`server is listening on port ${port}`));
+app.listen(port, () => console.log(`server is listening on port ${port}`))
